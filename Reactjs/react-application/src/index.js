18407d4023cea0eb0c6f89820cd47568e078f946
LeeCatherine <CatherineLee@Catherines-MacBook-Pro.local>
2020-10-10 16:41:50 +0800

    create react application

---
+++ Reactjs/react-application/src/index.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import './index.css';
+import Navbar from './navbar';
+import Home from './home/Home';
+import Games from './games/Games';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/games">
+            <Games />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
